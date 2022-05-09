95d8bda0d66ea46549b13ba0e9e3a899e2c62962
NaughtySwappie <koetsetijmen@gmail.com>
2022-05-09 23:18:59 +0200

    commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+{
+    const textRoulette = () => {
+        const words = [`design`, `develop`, `learning`];
+        let text = document.querySelector(`.roulette`);
+        let i = 0;
+        const interval = setInterval(function() {
+            text.innerHTML = words[i];
+            i++;
+            if(i >= 3)
+                i = 0;
+          }, 2000);
+    }
+
+    const init = () => {
+        textRoulette();
+    }
+
+    init();
+}
